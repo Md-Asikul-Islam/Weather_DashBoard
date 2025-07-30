b7465dfcfa42b4865877737f593c76033a667b1d
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-30 13:40:29 +0600

    feat: refactor Header by extracting FavoriteListModal into separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "./Header/Header";
 
+
 const App = () => {
   return (
-    <>
+    <div className="grid place-items-center h-screen">
       <Header />
-    </>
+    </div>
   );
 };
 
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,25 +2,17 @@ import React from "react";
 import Logo from "./Logo";
 import Search from "./Search";
 import Favorite from "./Favorite";
+import FavoriteListModal from "./FavoriteListModal";
 
 const Header = () => {
   return (
     <header className="fixed w-full top-0 z-50 bg-gradient-to-b from-black/60 to-black/0 pb-10">
-      <nav className="container flex items-center justify-between py-6">
+      <nav className="container mx-auto flex items-center justify-between py-6">
         <Logo />
-
         <div className="flex items-center gap-4 relative">
-           <Search />
-           <Favorite />
-
-          <div className="max-w-xs py-4 bg-white rounded-md border-gray-500 absolute right-0 top-16 text-black shadow-lg ">
-            <h3 className="text-lg font-bold px-4">Favourite Locations</h3>
-            <ul className="space-y-2 mt-4 *:py-2 *:px-4 *:cursor-pointer">
-              <li className="hover:bg-gray-200">Dhaka</li>
-              <li className="hover:bg-gray-200">Rangpur</li>
-              <li className="hover:bg-gray-200">Europe</li>
-            </ul>
-          </div>
+          <Search />
+          <Favorite />
+          <FavoriteListModal />
         </div>
       </nav>
     </header>
