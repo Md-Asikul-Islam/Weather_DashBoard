2a955087303ed1b9ef98256523e90efcf15a1527
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-30 23:21:53 +0600

    feat: refactor AddToFavorite by extracting Weather Board  into separate component

--- src/Weather/AddToFavorite.jsx
+++ src/Weather/AddToFavorite.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import heartIcon from "../assets/heart.svg"
 const AddToFavorite = () => {
   return (
-    <div>AddToFavorite</div>
-  )
-}
+    <div className="flex items-center justify-end space-x-6">
+      <button className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D]">
+        <span>Add to Favourite</span>
+        <img src={heartIcon} alt="heartIcon" />
+      </button>
+    </div>
+  );
+};
 
-export default AddToFavorite
+export default AddToFavorite;
