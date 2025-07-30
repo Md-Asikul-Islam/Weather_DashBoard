93713a87fb1e972cf8507118704792b6f0f5bbe5
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-30 23:32:03 +0600

    feat: refactor Weather condition by extracting Weather Board  into separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./Header/Header";
-
+import WeatherBoard from "./Weather/WeatherBoard";
 
 const App = () => {
   return (
     <div className="grid place-items-center h-screen">
       <Header />
+      <main>
+        <section>
+          <WeatherBoard />
+        </section>
+      </main>
     </div>
   );
 };
