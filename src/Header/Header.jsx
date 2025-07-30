6344f7416b270b0e4931fbd113476f15e1d33e05
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-30 13:33:19 +0600

    feat: refactor Header by extracting Favorite into separate component

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Logo from "./Logo";
 import Search from "./Search";
+import Favorite from "./Favorite";
 
 const Header = () => {
   return (
@@ -10,10 +11,7 @@ const Header = () => {
 
         <div className="flex items-center gap-4 relative">
            <Search />
-          <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
-            <img src="./assets/heart.svg" alt="" />
-            <span>Favourite Locations</span>
-          </div>
+           <Favorite />
 
           <div className="max-w-xs py-4 bg-white rounded-md border-gray-500 absolute right-0 top-16 text-black shadow-lg ">
             <h3 className="text-lg font-bold px-4">Favourite Locations</h3>
